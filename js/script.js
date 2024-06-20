8484c10892cee4cfb4154170c6c3f2a2b688aa5c
Pawel Owiesek <Owiesek.p@gmail.com>
2024-06-20 08:38:22 +0200

    add script to calculate BMI

---
+++ js/script.js
@@ -0,0 +1,19 @@
+{
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    const height = document.querySelector(".js-height").value;
+    const weight = document.querySelector(".js-weight").value;
+    const score = document.querySelector(".js-score");
+    let result;
+    result = weight / (height / 100) ** 2;
+    score.innerText = result.toFixed(0);
+    console.log(height);
+  };
+
+  const execute = () => {
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  execute();
+}
